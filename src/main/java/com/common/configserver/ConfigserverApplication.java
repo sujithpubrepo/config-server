a4124a4c2ac9ad0339a56ff05d7dfd64c1d00696
sujithkumarmp <sujithkumarmp@gmail.com>
2023-08-21 21:12:48 -0400

    commit

--- src/main/java/com/common/configserver/ConfigserverApplication.java
+++ src/main/java/com/common/configserver/ConfigserverApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 
